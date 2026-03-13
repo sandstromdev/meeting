2e8126686566d87177a85ca5e7271c7519dc3bd6
Leo Sandström <leonsandstrom@gmail.com>
2026-03-13 22:55:38 +0100

    wip

--- src/lib/server/convex.ts
+++ src/lib/server/convex.ts
@@ -1,17 +1,21 @@
 import { getRequestEvent } from '$app/server';
 import { env } from '$env/dynamic/public';
-import { createConvexHttpClient } from '@mmailaender/convex-better-auth-svelte/sveltekit';
+import { ConvexHttpClient } from 'convex/browser';
 
 export function getConvexClient(token?: string) {
-	const event = getRequestEvent();
+	const { locals, fetch } = getRequestEvent();
 
-	const client = createConvexHttpClient({
-		convexUrl: env.PUBLIC_CONVEX_URL,
-		token: token ?? event.locals.token,
-		options: {
-			logger: true,
-		},
+	const auth = token ?? locals.token;
+
+	const client = new ConvexHttpClient(env.PUBLIC_CONVEX_URL, {
+		auth,
+		logger: true,
+		fetch: fetch,
 	});
+
+	if (token) {
+		client.setAuth(token);
+	}
 
 	return client;
 }
