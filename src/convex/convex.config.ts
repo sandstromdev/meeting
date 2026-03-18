17536e2692e9a905a068e391406232e3648efbf3
sandstrom <leonsandstrom@gmail.com>
2026-03-18 08:18:40 +0100

    removed sharded counter component from convex

--- src/convex/convex.config.ts
+++ src/convex/convex.config.ts
@@ -1,12 +1,10 @@
 import { defineApp } from 'convex/server';
 import betterAuth from './betterAuth/convex.config';
-import counter from '@convex-dev/sharded-counter/convex.config';
 import counters from './counter/convex.config';
 
 const app = defineApp();
 
 app.use(betterAuth);
-app.use(counter);
 app.use(counters);
 
 export default app;
