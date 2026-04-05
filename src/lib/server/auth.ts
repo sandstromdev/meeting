85ad0339ddb53e70d3d38c2000e84571f240f89f
Leo Sandström <leonsandstrom@gmail.com>
2026-04-05 23:10:54 +0200

    move auth hook to $lib/server/auth.ts

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,8 +1,15 @@
 import { getRequestEvent } from '$app/server';
 import { api } from '$convex/_generated/api';
-import { type RequestEvent } from '@sveltejs/kit';
+import { type Handle, type RequestEvent } from '@sveltejs/kit';
 import { getConvexClient } from './convex';
 import { assertAuthed } from './guards';
+import { createAuth } from '$convex/auth';
+import * as privateEnv from '$env/static/private';
+import * as publicEnv from '$env/static/public';
+import { getSecureCookieName } from '$lib/server/cookie';
+import { getMeetingCookie } from '$lib/server/meeting-cookie';
+import { getToken } from '@mmailaender/convex-better-auth-svelte/sveltekit';
+import { withServerConvexToken } from '@mmailaender/convex-svelte/sveltekit/server';
 
 export function getAuthedRequestEvent() {
 	const event = getRequestEvent();
@@ -23,3 +30,42 @@ export async function getCurrentUser() {
 
 	return await client.query(api.app.me.getCurrentUser, {}).catch(() => null);
 }
+
+export const handleAuth = (async ({ event, resolve }) => {
+	// Merge vite-loaded environment variables into process.env,
+	// otherwise Better Auth will complain about missing variables.
+	Object.assign(process.env, {
+		...privateEnv,
+		...publicEnv,
+	});
+
+	// Get the session token from the cookies.
+	const sessionToken = event.cookies.get(getSecureCookieName('better-auth.session_token'));
+
+	// Get the convex_jwt cookie from cookies.
+	let token = await getToken(createAuth, event.cookies);
+
+	// If the token is not found, try to get a new one from the session token.
+	if (!token && sessionToken && event.route.id !== '/api/auth/[...all]') {
+		const response = await event.fetch('/api/auth/get-session', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		});
+
+		// If the response is ok, get the token from the set-cookie header. If response is not ok, ignore the error.
+		if (response.ok) {
+			const cookie = response.headers
+				.getSetCookie()
+				.find((c) => c.startsWith(getSecureCookieName('better-auth.convex_jwt=')));
+			token = cookie?.split('=').at(1)?.split(';').at(0);
+		}
+	}
+
+	event.locals.token = token;
+	event.locals.meetingId = getMeetingCookie(event.cookies);
+
+	return withServerConvexToken(token, () => resolve(event));
+}) satisfies Handle;
