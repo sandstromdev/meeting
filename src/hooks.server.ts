0d7c6deb38ad1ddef48732f67fffa296755f9ccb
sandstrom <leonsandstrom@gmail.com>
2026-04-16 12:45:46 +0200

    under construction message

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,5 @@
 import { handleAuth } from '$lib/server/auth';
+import { handleUnderConstruction } from '$lib/server/handle-under-construction';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = sequence(handleAuth);
+export const handle = sequence(handleUnderConstruction, handleAuth);
