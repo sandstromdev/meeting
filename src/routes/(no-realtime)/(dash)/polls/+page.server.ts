a5109579259031153988598a994fbe0506e641e3
sandstrom <leonsandstrom@gmail.com>
2026-04-16 13:05:48 +0200

    move /polls

---
+++ src/routes/(no-realtime)/(dash)/polls/+page.server.ts
@@ -0,0 +1,12 @@
+import { redirectIfNotAuthed } from '$lib/server/guards';
+import { convexLoad } from '@mmailaender/convex-svelte/sveltekit';
+import type { PageServerLoad } from './$types';
+import { api } from '$convex/_generated/api';
+
+export const load = (async ({ url }) => {
+	redirectIfNotAuthed(`/sign-in?redirect=${encodeURIComponent(url.pathname)}`);
+
+	return {
+		currentUser: await convexLoad(api.app.me.getCurrentUser, {}),
+	};
+}) satisfies PageServerLoad;
--- src/routes/(realtime)/(dash)/polls/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-	return {
-		// polls: await convexLoad(api.userPoll.public.getMyOwnedPolls, {}),
-	};
-}) satisfies PageServerLoad;
